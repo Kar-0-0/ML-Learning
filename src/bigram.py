e12b8646850f228135c6717970787b57a330fe2a
Kartik Mehta <kartikmehta0802@gmail.com>
2025-11-20 17:23:01 -0800

    added transformer

--- src/bigram.py
+++ src/bigram.py
@@ -2,6 +2,17 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+epochs = 5000
+n_layers = 6
+context_size = 256
+batch_size = 64
+learning_rate = 3e-4
+num_heads = 6
+n_emb = 384
+head_size = n_emb // num_heads
+dropout = .2
+device = 'mps'
+
 with open('data/more.txt', 'r') as f:
     data = f.read()
 
@@ -20,9 +31,6 @@ n = int(len(x) * .9)
 train_x = x[:n]
 test_x = x[n:]
 
-context_size = 8
-batch_size = 64
-
 
 def get_batch(split, batch_size):
     data = train_x if split == 'train' else test_x
@@ -34,14 +42,90 @@ def get_batch(split, batch_size):
     return x, y
 
 
+class Head(nn.Module):
+    def __init__(self, n_emb, head_size):
+        super().__init__()
+        self.query = nn.Linear(n_emb, head_size, bias=False)
+        self.key = nn.Linear(n_emb, head_size, bias=False)
+        self.value = nn.Linear(n_emb, head_size, bias=False)
+        self.register_buffer('tril', torch.tril(torch.ones((context_size, context_size))))
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        _, T, C = x.shape
+        q = self.query(x) # B, T, 16
+        k = self.key(x) # B, T, 16
+        v = self.value(x) # B, T, 16
+
+        wei = q @ k.transpose(-2, -1) * C**-0.5 # B, T, T
+        wei = wei.masked_fill(self.tril[:T, :T] == 0, float('-inf'))
+        wei = F.softmax(wei, dim=-1)
+        wei = self.dropout(wei)
+
+        out = wei @ v # B, T, hs
+        return out 
+
+
+class MultiHeadAttention(nn.Module):
+    def __init__(self, num_heads, head_size):
+        super().__init__()
+        self.heads = nn.ModuleList([Head(n_emb, head_size) for _ in range(num_heads)])
+        self.proj = nn.Linear(num_heads*head_size, n_emb)
+        self.dropout = nn.Dropout(dropout)
+    def forward(self, x):
+        out = torch.cat([h(x) for h in self.heads], dim=-1)        
+        out = self.proj(out)
+        out = self.dropout(out)
+        
+        return out
+
+class FeedForward(nn.Module):
+    def __init__(self, n_emb):
+        super().__init__()
+        self.net = nn.Sequential(
+            nn.Linear(n_emb, 4*n_emb),
+            nn.ReLU(),
+            nn.Linear(4*n_emb, n_emb),
+            nn.Dropout(dropout)
+        )
+
+    def forward(self, x):
+        return self.net(x)
+
+
+class Block(nn.Module):
+    def __init__(self, n_emb, n_head):
+        super().__init__()
+        head_size = n_emb // n_head
+        self.sa = MultiHeadAttention(n_head, head_size)
+        self.ffwd = FeedForward(n_emb)
+        self.ln1 = nn.LayerNorm(n_emb)
+        self.ln2 = nn.LayerNorm(n_emb)
+    
+    def forward(self, x):
+        x = x + self.sa(self.ln1(x))
+        x = x + self.ffwd(self.ln2(x))
+
+        return x
+
 class BigramLanguageModel(nn.Module):
     def __init__(self, embed_dim):
         super().__init__()
-        self.embed_table = nn.Embedding(embed_dim, embed_dim)
+        self.tok_emb = nn.Embedding(vocab_size, embed_dim)
+        self.pos_emb = nn.Embedding(context_size, embed_dim)
+        self.blocks = nn.Sequential(*[Block(n_emb, num_heads) for _ in range(n_layers)])
+        self.ln = nn.LayerNorm(n_emb)
+        self.lm_head = nn.Linear(n_emb, vocab_size)
 
     def forward(self, x, target=None):
         # x is (B, T)
-        logits = self.embed_table(x) # (B, T) --> (B, T, C)
+        _, T = x.shape
+        tok_embs = self.tok_emb(x) # (B, T) --> (B, T, C)
+        pos_embs = self.pos_emb(torch.arange(T)) # (T, C)
+        x = tok_embs + pos_embs
+        x = self.blocks(x)
+        x = self.ln(x)
+        logits = self.lm_head(x)
 
         if target is not None:
             B, T, C = logits.shape
@@ -56,7 +140,7 @@ class BigramLanguageModel(nn.Module):
 
     def generate(self, idx, max_tokens):
         for _ in range(max_tokens):
-            logits, _ = self(idx)
+            logits, _ = self(idx[:, -context_size:])
             logits = logits[:, -1, :]
             probs = F.softmax(logits, dim=-1)
             new_idx = torch.multinomial(probs, num_samples=1, replacement=True)
@@ -64,25 +148,20 @@ class BigramLanguageModel(nn.Module):
         return idx
 
 
-model = BigramLanguageModel(vocab_size)
+model = BigramLanguageModel(n_emb)
 
 idx = torch.zeros((1, 1), dtype=torch.long)
 
-print("Before training: ")
-print(decode(model.generate(idx, 200)[0].tolist()))
-print("-----------------------------------------------------")
-
-epochs = 1000
-optimizer = torch.optim.AdamW(model.parameters(), lr=1e-3)
+optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
 for epoch in range(epochs):
     xb, yb = get_batch('train', batch_size)
     logits, loss = model(xb, yb)
     
     if epoch % 100 == 0 or epoch+1 == epochs:
-        print(f"Epoch {epoch} Loss: {loss.item()}")
+        print(f"Epoch {epoch+1} Loss: {loss.item()}")
     
-    model.zero_grad()
+    optimizer.zero_grad()
     loss.backward()
     optimizer.step()
 
@@ -90,7 +169,7 @@ for epoch in range(epochs):
 idx = torch.zeros((1, 1), dtype=torch.long)
 
 print("After training: ")
-print(decode(model.generate(idx, 200)[0].tolist()))
+print(decode(model.generate(idx, 1000)[0].tolist()))
 
 
 
