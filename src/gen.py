5f5b5e833bac6038c5afffcdf3600e758096f056
Kartik Mehta <kartikmehta0802@gmail.com>
2025-11-30 03:07:08 -0800

    Final architecture

--- src/gen.py
+++ src/gen.py
@@ -3,19 +3,19 @@ import torch.nn as nn
 class Generator(nn.Module):
     def __init__(self):
         super().__init__()
-        self.l1 = nn.Linear(100, 3136) # (64x7x7)
-        self.conv1 = nn.ConvTranspose2d(64, 32, 4, 2, 1)
-        self.bn = nn.BatchNorm2d(32)
-        self.relu = nn.ReLU()
-        self.conv2 = nn.ConvTranspose2d(32, 16, 4, 2, 1)
-        self.bn2 = nn.BatchNorm2d(16)
-        self.conv3 = nn.Conv2d(16, 1, 7, padding=3)
+        self.l1 = nn.Linear(100, 256 * 8 * 8)   # 256x8x8
+        self.conv1 = nn.ConvTranspose2d(256, 128, 4, 2, 1)  # 128x16x16
+        self.bn1 = nn.BatchNorm2d(128)
+        self.conv2 = nn.ConvTranspose2d(128, 64, 4, 2, 1)   # 64x32x32
+        self.bn2 = nn.BatchNorm2d(64)
+        self.conv3 = nn.Conv2d(64, 3, 3, padding=1)         # 3x32x32
+        self.relu = nn.ReLU(True)
         self.tanh = nn.Tanh()
 
     def forward(self, x):
         x = self.l1(x)
-        x = x.view(x.size(0), 64, 7, 7)
-        x = self.relu(self.bn(self.conv1(x)))
+        x = x.view(x.size(0), 256, 8, 8)
+        x = self.relu(self.bn1(self.conv1(x)))
         x = self.relu(self.bn2(self.conv2(x)))
         x = self.tanh(self.conv3(x))
         return x
